c99ea8bc7a0342d2868a79e9636c8b457778deff
الحسن علي الكيلاني <hasankeylani@alMacBook-Air-alkhas-balhsn.local>
2025-05-04 20:56:14 +0300

    Initial project files and setup

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -1,5 +1,9 @@
 from app.db.database import SessionLocal
 
+from fastapi import Depends, HTTPException
+from app.core.security import get_current_user_oauth2
+from app.models.models import User
+
 # ✅ Shared DB session dependency
 def get_db():
     db = SessionLocal()
@@ -7,3 +11,10 @@ def get_db():
         yield db
     finally:
         db.close()
+
+def check_role(allowed_roles: list[str]):
+    def role_checker(user: User = Depends(get_current_user_oauth2)):
+        if user.role not in allowed_roles:
+            raise HTTPException(status_code=403, detail="You don't have permission to access this resource.")
+        return user
+    return role_checker
