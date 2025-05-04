9955c23a3e8311f33744778c08b681959f9aa00b
الحسن علي الكيلاني <hasankeylani@alMacBook-Air-alkhas-balhsn.local>
2025-05-04 13:26:58 +0300

    Initial project files and setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.db.database import engine
+from app.models import models
 
 app = FastAPI()
 
+# Create DB tables
+models.Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 async def root():
-    return {"message": "Welcome to TrendYol Backend with FastAPI!"}
+    return {"message": "TrendYol Backend with FastAPI & PostgreSQL is ready!"}
---
+++ backend/app/models/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from app.db.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+class Seller(Base):
+    __tablename__ = "sellers"
+
+    id = Column(Integer, primary_key=True, index=True)
+    store_name = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_verified = Column(Boolean, default=False)
