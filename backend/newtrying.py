71a8beaf53e7ce63db75ba50780ee6f9e5a4278e
الحسن علي الكيلاني <hasankeylani@alMacBook-Air-alkhas-balhsn.local>
2025-05-07 11:16:07 +0300

    Initial project files and setup

--- backend/newtrying.py
+++
@@ -1,7 +0,0 @@
-from jose import jwt
-
-token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJxYXp3c3hAZXhhbXBsZS5jb20iLCJleHAiOjE3NDY1OTczMTF9.vwjGgWBgd4ohg0OjHhmrvEpIcx9RWGqZkDsEvJvpIPY"
-secret = "XhPrTkgnivtBj4lehYrij4klyE7WDYB5ZOg92U1XRL0="
-
-payload = jwt.decode(token, secret, algorithms=["HS256"])
-print(payload)
